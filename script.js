0c694f145d6153349ef8b8a57a09a04596d189a4
krishnasairayavaram <krishnasairayavaram2005@gmail.com>
2025-01-20 20:58:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var randomNumber1=Math.floor(((Math.random())*6))+1;
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+var randomNumber2=Math.floor(((Math.random())*6))+1;
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+if(randomNumber1>randomNumber2){
+    document.querySelector(".win").innerHTML="🚩 Player 1 wins!";
+}else if(randomNumber1<randomNumber2){
+    document.querySelector(".win").innerHTML="Player 2 wins! 🚩";
+}else{
+    document.querySelector(".win").innerHTML="Draw!";
+}
